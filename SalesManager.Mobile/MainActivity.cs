935a4e2481f8812b552195aadefd8a2f22d324da
isaacdelarosa <isaacdelarosamendez@gmail.com>
2019-03-29 16:45:32 -0600

    INICIO

--- SalesManager.Mobile/MainActivity.cs
+++ SalesManager.Mobile/MainActivity.cs
@@ -18,6 +18,7 @@ namespace SalesManager.Mobile
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
 
+
             Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
             SetSupportActionBar(toolbar);
 
